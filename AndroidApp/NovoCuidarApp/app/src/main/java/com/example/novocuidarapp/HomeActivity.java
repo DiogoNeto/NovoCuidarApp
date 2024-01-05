f380c397c0a88130cc92e56916dbdc8c2153bed0
Diogo Conceicao <HCC-DConceicao@MBPLC.com>
2024-01-05 00:56:57 +0000

    poc login

---
+++ AndroidApp/NovoCuidarApp/app/src/main/java/com/example/novocuidarapp/HomeActivity.java
@@ -0,0 +1,30 @@
+package com.example.novocuidarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        back = (Button) findViewById(R.id.back);
+
+
+    back.setOnClickListener(new View.OnClickListener() {
+        // @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        }
+    });
+
+    }
+}
